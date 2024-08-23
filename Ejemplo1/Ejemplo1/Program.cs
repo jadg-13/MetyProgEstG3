c6b73834c9116dd76777a6bc31b0eef65ecaaffc
jadg13 <jadg13@hotmail.com>
2024-08-23 08:39:46 -0600

    Sesión 2

---
+++ Ejemplo1/Ejemplo1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Ejemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 13;
+            float x = 10.226f;
+             Console.WriteLine("Hello, World!");
+            Console.WriteLine($"El numero es {num}");
+
+        }
+    }
+}
